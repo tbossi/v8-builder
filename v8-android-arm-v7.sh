0e1febe1d864c33a151b7516200c23eb2c8ce2f8
Tommaso Bossi <tommasobossi95+github@gmail.com>
2020-06-03 17:56:01 +0200

    Add Android platforms

---
+++ v8-android-arm-v7.sh
@@ -0,0 +1,50 @@
+VERSION=$1
+
+sudo apt-get install -y \
+    pkg-config \
+    git \
+    subversion \
+    curl \
+    wget \
+    build-essential \
+    python \
+    xz-utils \
+    zip
+
+git config --global user.name "V8 Android Builder"
+git config --global user.email "v8.android.builder@localhost"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+
+cd ~
+echo "=====[ Getting Depot Tools ]====="	
+git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$(pwd)/depot_tools:$PATH
+gclient
+
+
+mkdir v8
+cd v8
+
+echo "=====[ Fetching V8 ]====="
+fetch v8
+echo "target_os = ['android']" >> .gclient
+cd ~/v8/v8
+./build/install-build-deps-android.sh
+git checkout $VERSION
+gclient sync
+
+
+echo "=====[ Building V8 ]====="
+python ./tools/dev/v8gen.py arm.release -vv -- '
+target_os = "android"
+target_cpu = "arm"
+v8_target_cpu = "arm"
+is_component_build = true
+v8_enable_i18n_support = false
+symbol_level = 1
+'
+ninja -C out.gn/arm.release -t clean
+ninja -C out.gn/arm.release v8
