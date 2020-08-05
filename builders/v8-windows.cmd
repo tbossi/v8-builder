69e109af93bc38aa50ea2e115fcb54b305eee079
Tommaso Bossi <tommasobossi95+github@gmail.com>
2020-08-05 15:58:26 +0200

    Fix windows build

--- builders/v8-windows.cmd
+++ builders/v8-windows.cmd
@@ -27,7 +27,7 @@ call gclient sync
 
 
 echo =====[ Building V8 ]=====
-call python .\tools\dev\v8gen.py x64.release -vv -- target_os="""win""" is_component_build=true use_custom_libcxx=false v8_enable_i18n_support=false symbol_level=0
+call python .\tools\dev\v8gen.py x64.release -vv -- target_os="""win""" is_component_build=true use_custom_libcxx=false is_clang=false use_lld=false v8_enable_i18n_support=false symbol_level=0
 
 call ninja -C out.gn\x64.release -t clean
 call ninja -C out.gn\x64.release v8
